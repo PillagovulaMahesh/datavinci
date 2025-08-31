b4fbe236450000bd62d9acf77b293ea114e65fea
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-08-31 11:10:24 +0530

    Update  App.js

---
+++ datavinci/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import './styles/App.css';
+import './styles/Responsive.css';
+import './styles/Navbar.css';
+import './styles/NestedCheckbox.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
