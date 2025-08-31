cdede0a570067fa2067a96f75185a280f329a740
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-08-31 11:05:05 +0530

    Update Header.js

---
+++ datavinci/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>DataVinci Frontend Assignment</h1>
+    </header>
+  );
+}
+
+export default Header;
